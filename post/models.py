83a74f7377dfe2a3148732681dcd810e958df2c6
Seamile <lanhuermao@gmail.com>
2018-04-28 11:00:03 +0800

    添加了帖子创建、修改和阅读

--- post/models.py
+++ post/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=64)
+    create = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
--- post/views.py
+++ post/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from post.models import Post
+
+
+def create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect('/post/read/?post_id=%s' % post.id)
+    else:
+        return render(request, 'create.html')
+
+
+def edit(request):
+    if request.method == 'POST':
+        # 取出 post
+        post_id = int(request.POST.get('post_id', 1))
+        post = Post.objects.get(id=post_id)
+        # 更新数据
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%s' % post.id)
+    else:
+        post_id = int(request.GET.get('post_id', 1))
+        post = Post.objects.get(id=post_id)
+        return render(request, 'edit.html', {'post': post})
+
+
+def read(request):
+    post_id = int(request.GET.get('post_id', 1))
+    post = Post.objects.get(id=post_id)
+    return render(request, 'read.html', {'post': post})
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
+
+
+def search(request):
+    return render(request, 'search.html', {})
